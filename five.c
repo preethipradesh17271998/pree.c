7b9f91d2aa7418af2f73a1fba2a288e46c6d2a6f
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-02-02 11:30:10 +0000

    Create five.c

---
+++ five.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,a[5],r,x;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				r=a[i];
+				a[i]=a[j];
+				a[j]=r;
+			}
+		}
+}
+x=(n+1)/2;
+printf("the madian element is %d",x);
+return 0;
+}
