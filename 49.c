b093001baf54a17709660838e8ed0cecb277bede
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-02-06 19:24:59 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the number ");
+    scanf("%d",&a);
+    printf("enter the number ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("the sum is %d",c);
+}
