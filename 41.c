072608069cccd91e6d71c6b8309abe3cc996a3ee
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-02-07 10:47:11 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int i,n=3;
+    char a[10];
+    printf("enter the string ");
+    scanf("%s",&a);
+    for(i=0;i<n;i++)
+    {
+        printf("%s \n",a);
+    }
