515ae7d9587e8abf67a3bc7094681681ed5b6528
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-02-07 11:20:02 +0000

    Update line.c

--- line.c
+++ line.c
@@ -2,10 +2,10 @@
 void main()
 {
     int i,c=0,n;
-    char a[50]="viji is good.123";
+    char a[50]="viji is good.123.";
     for(i=0;a[i]!='\0';i++)
 {
-    if(a[i]==' ')
+    if(a[i]=='.')
     {
         c++;
     }
