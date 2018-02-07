c74965699ffe495e9098d4749bac5070e6c4e2ad
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-02-07 11:29:50 +0000

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+   char a[10],b[10];
+   printf("enter the string ");
+   scanf("%s",&a);
+   printf("enter the string ");
+   scanf("%s",&b);
+   printf("%s%s",a,b);
+}
