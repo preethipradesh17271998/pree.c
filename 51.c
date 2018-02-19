db95a69e796386b5c7b7528894edb3a066c903fa
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-02-19 16:42:07 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a[30],n=3,i;
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        printf(" %d",a[i]);
+    }
+}
