f24d49bdb16e9f6ff8a657886d7666fd109901c2
preethipradesh17271998 <31505560+preethipradesh17271998@users.noreply.github.com>
2018-02-19 19:36:28 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,n1,n2;
+    printf("enter the values");
+    scanf("%d%d%d",&a,&n1,&n2);
+    if(a>n1&&a<n2)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
